8b3f0a81115b7ce29cabcc362115a884d643092e
MiltonDavila2 <miltondaviladt@gmail.com>
2025-10-10 01:19:26 -0500

    Tercer Video

---
+++ src/main/java/org/example/OpenXava/modelo/Factura.java
@@ -0,0 +1,46 @@
+package org.example.OpenXava.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.example.OpenXava.calculadores.CalculadorSiguienteNumeroParaAnyo;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import org.openxava.calculators.CurrentLocalDateCalculator;
+import org.openxava.calculators.CurrentYearCalculator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Collection;
+
+@Entity @Getter @Setter
+public class Factura {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @Hidden
+    @GenericGenerator(name = "system-uuid",strategy = "uuid")
+    @Column(length = 32)
+    String oid;
+
+    @DefaultValueCalculator(CurrentYearCalculator.class)
+    @Column(length = 4)
+    int anyo;
+
+    @Column(length = 6)
+    @DefaultValueCalculator(value = CalculadorSiguienteNumeroParaAnyo.class,
+                           properties = @PropertyValue(name="anyo"))
+    int numero;
+
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class)
+    LocalDate fecha;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    Cliente cliente;
+
+    @ElementCollection @ListProperties("producto.numero, producto.descripcion, cantidad")
+    Collection<Detalle> detalles;
+
+    @Stereotype("MEMO")
+    String Observaciones;
+}
