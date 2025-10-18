0836bea76e0ed7095b2e6bcfb6c3949757179ae7
MiltonDavila2 <miltondaviladt@gmail.com>
2025-10-17 22:51:16 -0500

    Todo

--- src/main/java/org/example/OpenXava/run/OpenXava.java
+++ src/main/java/org/example/OpenXava/run/OpenXava.java
@@ -11,7 +11,7 @@ import org.openxava.util.*;
 public class OpenXava {
 
 	public static void main(String[] args) throws Exception {
-		DBServer.start("OpenXava-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		//DBServer.start("OpenXava-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
 		AppServer.run("OpenXava"); // Usa AppServer.run("") para funcionar en el contexto raíz
 	}
 
